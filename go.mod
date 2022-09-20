c03ac4157a59fc6ef55cb559c5ded78d84727993
abhiskumar <abhiskumar@teksystems.com>
2022-09-20 20:56:09 +0530

    Added

--- go.mod
+++ go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/rs/cors v1.8.2 // indirect
 )
--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 
 	"mysql-crud/controller"
 )
@@ -25,11 +25,13 @@ func main() {
 	router.HandleFunc("/addUser", controller.AddUser).Methods("POST")
 	router.HandleFunc("/getUser/{email}", controller.GetUser).Methods("GET")
 
+	handler := cors.Default().Handler(router)
+
 	// router.HandleFunc("/getBooks", controller.AllBooks).Methods("GET")
 	// router.HandleFunc("/getBook/{id}", controller.FindBook).Methods("GET")
 	// router.HandleFunc("/addBook", controller.AddBook).Methods("POST")
 	// router.HandleFunc("/updateBook/{id}", controller.UpdateBook).Methods("PUT")
 	// router.HandleFunc("/deleteBook/{id}", controller.DeleteBook).Methods("DELETE")
 
-	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS()(router)))
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
